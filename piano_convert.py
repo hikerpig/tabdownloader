e3a273553e306154757d6fa2186459ef5f682fb1
hikerpig <102706325@163.com>
2014-12-10 16:10:01 +0800

    Tab pics converter

--- piano_convert.py
+++ piano_convert.py
@@ -2,26 +2,37 @@
 #!/usr/bin/env python
 from PIL import Image
 import os
-#import ipdb
+import argparse
+
+parser = argparse.ArgumentParser(description="Tab pics converter for ChongChong Music")
+parser.add_argument('-i', action="store", dest="input_dir", default='.', help="Input directory")
+parser.add_argument('-o', action="store", dest="output_dir", default='output', help="Output directory")
 
 
 ALLOW_EXT = [
-    'gif',
-    'png',
-    ]
+  'gif',
+  'png',
+]
 
 OUTPUT_DIR = 'output'
 
-def main():
-    """
-    把从虫虫钢琴网上下下来的gif转成背景白色的可以看的图片
-    """
+class TabConverter:
+  """
+  把从虫虫钢琴网上下下来的gif转成背景白色的可以看的图片
+  """
+  input_dir = '.'
+  output_dir = 'output'
 
-    raw_names = os.listdir('.')
-    #print raw_names
+  def __init__(self, input_dir, output_dir):
+    self.input_dir = input_dir
+    self.ouput_dir = output_dir
+
+  def convert(self):
+
+    raw_names = os.listdir(self.input_dir)
     names = []
-    if not os.path.isdir(OUTPUT_DIR):
-      os.mkdir(OUTPUT_DIR)
+    if not os.path.isdir(self.ouput_dir):
+      os.mkdir(self.ouput_dir)
 
     for name in raw_names:
       for ext in ALLOW_EXT:
@@ -29,12 +40,15 @@ def main():
           names.append(name)
           break
 
-
     for name in names:
       #cwd = os.getcwd()
       #name = os.path.join(cwd, 'input', name)
-      #ipdb.set_trace()
-      img = Image.open(name)
+
+      inputName = os.path.join(self.input_dir, name)
+      outputName = os.path.join(self.output_dir, name)
+      print "Processing ", name
+
+      img = Image.open(inputName)
       img2 = Image.new('RGBA', img.size, (255, 255, 255))
       img2.paste(img, img2)
       fmt = 'gif'
@@ -42,8 +56,12 @@ def main():
         if name.find(ext) > -1:
           fmt = ext
           break
-      img2.save('%s/%s'%(OUTPUT_DIR, name), fmt)
+      img2.save(outputName, fmt)
+      print "Output is", outputName
 
 if __name__ == '__main__':
-  main()
+  args = parser.parse_args()
+
+  converter = TabConverter(args.input_dir, args.output_dir)
+  converter.convert()
 
